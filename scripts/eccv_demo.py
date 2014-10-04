3c068a9fa74130bf697395ac52c9b97d6b856c0a
Kyle Wilson <kyle.lyndon.wilson@gmail.com>
2014-10-04 13:38:39 -0400

    fixed a weird indentation problem.

--- scripts/eccv_demo.py
+++ scripts/eccv_demo.py
@@ -12,8 +12,8 @@ import sys
 
 
 """
-This is a demo driver for the ECCV 2014 1DSfM / trans solver paper [1]. It demonstrates 
-how to call the methods and solvers in the included sfminit package, producing a robust 
+This is a demo driver for the ECCV 2014 1DSfM / trans solver paper [1]. It demonstrates
+how to call the methods and solvers in the included sfminit package, producing a robust
 solution to the SfM translations problem.
 
 [1] K. Wilson and N. Snavely. Robust Global Translations with 1DSfM. ECCV 2014.
@@ -27,60 +27,60 @@ def make_cli():
     return parser.parse_args()
 
 def run_pipeline():
-	args = make_cli()
-	data_dir = args.dataset_dir
-	out_dir = args.output_dir
-	input = {'EGs'     : os.path.join(data_dir, 'EGs.txt'),
-		     'ccs'     : os.path.join(data_dir, 'cc.txt'),
-		     'tracks'  : os.path.join(data_dir, 'tracks.txt'),
-		     'coords'  : os.path.join(data_dir, 'coords.txt'),
-		     'gt_soln' : os.path.join(data_dir, 'gt_bundle.out')   }
+    args = make_cli()
+    data_dir = args.dataset_dir
+    out_dir = args.output_dir
+    input = {'EGs'     : os.path.join(data_dir, 'EGs.txt'),
+         'ccs'     : os.path.join(data_dir, 'cc.txt'),
+         'tracks'  : os.path.join(data_dir, 'tracks.txt'),
+         'coords'  : os.path.join(data_dir, 'coords.txt'),
+         'gt_soln' : os.path.join(data_dir, 'gt_bundle.out')   }
 
-	if not os.path.isdir(out_dir):
-		print '[eccv_demo] Error: output dir does not exist! Aborting.'
-		sys.exit(1)
+    if not os.path.isdir(out_dir):
+        print '[eccv_demo] Error: output dir does not exist! Aborting.'
+        sys.exit(1)
 
-	################
-	### PIPELINE ###
-	################
+    ################
+    ### PIPELINE ###
+    ################
 
-	print '[eccv_demo] Reading input files'
-	cc = np.loadtxt(input['ccs'])
-	tracks = sfminit.Tracks.from_file(input['tracks'])
-	coords = sfminit.Coords.from_file(input['coords'])
-	if os.path.exists(input['gt_soln']): # skip comparisons if the gt isn't there
+    print '[eccv_demo] Reading input files'
+    cc = np.loadtxt(input['ccs'])
+    tracks = sfminit.Tracks.from_file(input['tracks'])
+    coords = sfminit.Coords.from_file(input['coords'])
+    if os.path.exists(input['gt_soln']): # skip comparisons if the gt isn't there
         gt_bundle = sfminit.Bundle.from_file(input['gt_soln'])
 
-	print '[eccv_demo] Running global rotations solver'
-	models = sfminit.ModelList.from_EG_file(input['EGs'])
-	edges, pairwise_rotations = models.get_rotations_problem() 
-	global_rot_ids, global_rots = sfminit.solve_global_rotations(edges, pairwise_rotations, cc=cc)
+    print '[eccv_demo] Running global rotations solver'
+    models = sfminit.ModelList.from_EG_file(input['EGs'])
+    edges, pairwise_rotations = models.get_rotations_problem()
+    global_rot_ids, global_rots = sfminit.solve_global_rotations(edges, pairwise_rotations, cc=cc)
 
-	print '[eccv_demo] Making a translations problem'
-	models.apply_rotations_solution(global_rot_ids, global_rots)
-	trans_problem = models.get_translations_problem()
-	trans_problem.add_track_edges(tracks, coords, global_rot_ids, global_rots)
+    print '[eccv_demo] Making a translations problem'
+    models.apply_rotations_solution(global_rot_ids, global_rots)
+    trans_problem = models.get_translations_problem()
+    trans_problem.add_track_edges(tracks, coords, global_rot_ids, global_rots)
 
-	print '[eccv_demo] Running 1DSfM to remove outliers'
-	clean_trans_problem = copy.deepcopy(trans_problem)
-	clean_trans_problem.run_1DSfM(verbose=True)
+    print '[eccv_demo] Running 1DSfM to remove outliers'
+    clean_trans_problem = copy.deepcopy(trans_problem)
+    clean_trans_problem.run_1DSfM(verbose=True)
 
-	print '[eccv_demo] Solving translations problem'
-	X_ids, X = clean_trans_problem.solve(loss=None, function_tol=1e-14, parameter_tol=1e-14, max_iterations=1000)
+    print '[eccv_demo] Solving translations problem'
+    X_ids, X = clean_trans_problem.solve(loss=None, function_tol=1e-14, parameter_tol=1e-14, max_iterations=1000)
 
-	###########################
-	### EVALUATION / OUTPUT ###
-	###########################
+    ###########################
+    ### EVALUATION / OUTPUT ###
+    ###########################
 
-	print '[eccv_demo] Writing output files'
-	if not os.path.isdir(out_dir):
-		os.mkdir(out_dir)
-	sfminit.write_rot_file(os.path.join(out_dir, 'rot_solution.txt'), global_rot_ids, global_rots)
-	trans_problem.write(os.path.join(out_dir, 'orig_trans_problem.txt'))
-	clean_trans_problem.write(os.path.join(out_dir, '1DSfM_trans_problem.txt'))
-	sfminit.write_trans_soln_file(os.path.join(out_dir, 'trans_problem_solution.txt'), X_ids, X)
+    print '[eccv_demo] Writing output files'
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
+    sfminit.write_rot_file(os.path.join(out_dir, 'rot_solution.txt'), global_rot_ids, global_rots)
+    trans_problem.write(os.path.join(out_dir, 'orig_trans_problem.txt'))
+    clean_trans_problem.write(os.path.join(out_dir, '1DSfM_trans_problem.txt'))
+    sfminit.write_trans_soln_file(os.path.join(out_dir, 'trans_problem_solution.txt'), X_ids, X)
 
-	if os.path.exists(input['gt_soln']):
+    if os.path.exists(input['gt_soln']):
         print '[eccv_demo] Running comparisons to ground truth'
         # get the common camera locations
         X_gt = []
@@ -96,7 +96,7 @@ def run_pipeline():
         residuals = np.sqrt(np.sum(np.square(X_gt - s*np.dot(R, X_soln.T).T - t), axis=1))
         np.savetxt(os.path.join(out_dir, 'trans_solution_gt_error.txt'), residuals)
 
-	print '[eccv_demo] Done!'
+    print '[eccv_demo] Done!'
 
 if __name__ == '__main__':
-	sys.exit(run_pipeline())
+    sys.exit(run_pipeline())
